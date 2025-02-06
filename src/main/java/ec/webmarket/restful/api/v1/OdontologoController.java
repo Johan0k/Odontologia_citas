6770d9aadb7053925546ac8a64c63dede9f93741
Johan0k <johanqpasoayer8@hotmail.com>
2025-02-06 17:56:48 -0500

    Clases odontologo

---
+++ src/main/java/ec/webmarket/restful/api/v1/OdontologoController.java
@@ -0,0 +1,75 @@
+package ec.webmarket.restful.api.v1;
+
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import ec.webmarket.restful.dto.v1.OdontologoDTO;
+
+import ec.webmarket.restful.security.ApiResponseDTO;
+import ec.webmarket.restful.service.crud.OdontologoService;
+import jakarta.validation.Valid;
+
+
+
+@RestController
+@RequestMapping("/api/v1/odontologo")
+public class OdontologoController {
+	
+
+		@Autowired
+		private OdontologoService entitysService;
+
+		
+		@GetMapping("/listar")
+		public ResponseEntity<?> getAll() {
+			return new ResponseEntity<>(new ApiResponseDTO<>(true, entitysService.findAll(new OdontologoDTO())), HttpStatus.OK);
+		}
+
+		
+		@PostMapping
+		public ResponseEntity<?> create(@RequestBody OdontologoDTO dto) {
+			return new ResponseEntity<>(new ApiResponseDTO<>(true, entitysService.create(dto)), HttpStatus.CREATED);
+		}
+		
+
+		@PutMapping
+		public ResponseEntity<?> update(@RequestBody OdontologoDTO dto) {
+			return new ResponseEntity<>(new ApiResponseDTO<>(true, entitysService.update(dto)), HttpStatus.OK);
+		}
+		
+
+		@GetMapping("/{id}")
+		public ResponseEntity<?> getById(@Valid @PathVariable Long id) {
+			OdontologoDTO dto = new OdontologoDTO();
+			dto.setId(id);
+			return new ResponseEntity<>(new ApiResponseDTO<>(true, entitysService.find(dto)), HttpStatus.OK);
+		}
+		
+		
+
+		@DeleteMapping("/{id}")
+		public ResponseEntity<?> deleteById(@PathVariable Long id) {
+		    OdontologoDTO dto = new OdontologoDTO();
+		    dto.setId(id);
+		    entitysService.delete(dto);
+		    return new ResponseEntity<>(new ApiResponseDTO<>(true, "Odontólogo eliminado"), HttpStatus.OK);
+		}
+		
+		
+
+		
+		}
+		
+
+
---
+++ src/main/java/ec/webmarket/restful/persistence/OdontologoRepository.java
@@ -0,0 +1,12 @@
+package ec.webmarket.restful.persistence;
+
+
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import ec.webmarket.restful.domain.Odontologo;
+
+public interface OdontologoRepository extends JpaRepository<Odontologo, Long>{
+
+	//List<Odontologo>
+}
